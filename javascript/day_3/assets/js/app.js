251623b223413d68e4e2b3362ac054390eca46d5
BhuwanDeveloper <khatribhuwan055@gmail.com>
2024-07-19 19:00:50 +1000

    Unsettled amount

--- javascript/day_3/assets/js/app.js
+++ javascript/day_3/assets/js/app.js
@@ -1,47 +1,44 @@
-console.log('calculator');
+console.log("calculator");
 // DOM - Document Object Model
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => {
-    button.addEventListener('click', handleClick)
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
 });
 
-let calculateExp = '';
+let calculateExp = "";
 
 function handleClick(event) {
-    const value = event.target.innerText;
-    switch(value){
-        case "=" :
-            try {
-                            calculateExp = eval(calculateExp);
-                        }
-                        catch{
-                          calculateExp = '';
-                        }
-        break;
-        case 'CE':
-            calculateExp = '';
-            break;
-        case 'DEL':
-            calculateExp = calculateExp.slice(0, calculateExp.length - 1);
-            break;
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-            const lastValue = calculateExp[calculateExp.length - 1]
-            if(parseInt(lastValue) == lastValue) {
-                calculateExp = calculateExp + value;
-            }
-            else {
-                calculateExp = calculateExp.slice(0, calculateExp.length - 1) + value;
-            }
-            break;
-        default:
-            calculateExp = calculateExp + value;
-    }
+  const value = event.target.innerText;
+  switch (value) {
+    case "=":
+      try {
+        calculateExp = eval(calculateExp);
+      } catch {
+        calculateExp = "";
+      }
+      break;
+    case "CE":
+      calculateExp = "";
+      break;
+    case "DEL":
+      calculateExp = calculateExp.slice(0, calculateExp.length - 1);
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      const lastValue = calculateExp[calculateExp.length - 1];
+      if (parseInt(lastValue) == lastValue) {
+        calculateExp = calculateExp + value;
+      } else {
+        calculateExp = calculateExp.slice(0, calculateExp.length - 1) + value;
+      }
+      break;
+    default:
+      calculateExp = calculateExp + value;
+  }
 
-
-    /*
+  /*
     if(value=== "=") {
         try {
            calculateExp = eval(calculateExp);
@@ -70,5 +67,5 @@ function handleClick(event) {
         calculateExp = calculateExp + value;
     }
     */
-    document.querySelector('.display').innerText = calculateExp;
+  document.querySelector(".display").innerText = calculateExp;
 }
---
+++ javascript/split_cost/asset/js/app.js
@@ -0,0 +1,119 @@
+function User(name, email, mobile, photo ) {
+    this.name = name;
+    this.email = email;
+    this.mobile = mobile;
+    this.photo = photo;
+}
+
+
+function Expense(description, amount) {
+    this.description = description;
+    this.amount = amount;
+    this.isSettled = false;
+    this.date = new Date();
+}
+
+function SplitCostApp() {
+    this.unsettledAmount = 20.00;
+    this.users = [];
+    this.expenses = [];
+    
+
+    this.displayUnsettledAmount = function() {
+        document.querySelector(".amount").textContent = `$${this.unsettledAmount}`; 
+    }
+
+    this.addUser = function(name, email, mobile, photo) {
+        const user = new User(name, email, mobile, photo);
+        this.users.push(user);
+
+        this.displayUsers();
+    }
+
+    this.displayUsers = function() {
+        let userElements = '';
+        for(let user of this.users) {
+            userElements += `<div><img src="${user.photo}" alt="${user.name}" /></div>`
+        }
+        document.querySelector(".users-wrapper").innerHTML = userElements;
+    }
+
+    this.displayExpenses = function() {
+        let expenseElements = '';
+        for(let expense of this.expenses) {
+            expenseElements += `
+                    <div class="expenses-item ${expense.isSettled ? 'settled' : ''}">
+                    <div>
+                        <span>${expense.description}</span>
+                        <span>$${expense.amount}</span>
+                    </div>
+                    <div class="date">${expense.date} </div>
+                </div>
+            `
+        }
+        document.querySelector('.expenses-wrapper').innerHTML = expenseElements;
+    }
+
+    this.addExpenses = function(event) {
+        event.preventDefault();
+        console.log('Adding expenses...');
+        const description = document.querySelector("#description").value;
+        const amount = document.querySelector("#amount").value;
+        if(description && amount) {
+            const expense = new Expense(description, amount);
+            this.expenses.unshift(expense);
+    
+            this.displayExpenses();
+            document.querySelector("form").reset();  
+            this.calculateUnsettledAmount();
+            this.displayUnsettledAmount();
+        }
+    }
+
+    this.calculateUnsettledAmount = function() {
+        let total = 0;
+        for(let expense of this.expenses) {
+            if(!expense.isSettled) {
+                total = total + Number(expense.amount);
+            }
+        }
+        const unsettledAmount = total / (this.users.length + this.addNewUseer.length);
+        this.unsettledAmount = unsettledAmount.toFixed(2);
+    }
+
+    this.addNewEventListener = function() {
+        document.querySelector("form").addEventListener('submit',  (event) => {
+            this.addExpenses(event);
+        });
+    }
+    this.addSettleNowEventListener = function() {
+        document.querySelector("#settleNowBtn").addEventListener("click", (event) => {
+            this.settleNow(event);
+        })
+    }
+
+    this.settleNow = function(event) {
+        console.log('Settling now!');
+        this.expenses = this.expenses.map(expense => {
+            return {...expense, isSettled: true};
+        });
+        this.displayExpenses();
+        this.calculateUnsettledAmount();
+        this.displayUnsettledAmount();
+    }
+    this.addNewUserEventListener = function() {
+        document.getElementById("addNewUser").addEventListener('click', () => {
+            const randomValue = parseInt(Math.random() *100);
+            this.addUser('Alex', 'alex@gmail.com', '0404045678', `https://randomuser.me/api/portraits/women/${randomValue}.jpg`);
+        })
+    }
+}
+
+const splitCostApp = new SplitCostApp();
+splitCostApp.addNewEventListener();
+splitCostApp.addSettleNowEventListener();
+splitCostApp.displayUnsettledAmount();
+splitCostApp.addUser('Alex', 'alex@gmail.com', '0420400168', 'https://randomuser.me/api/portraits/men/90.jpg');
+splitCostApp.addUser('Jet', 'jet@gmail.com', '0420400162', 'https://randomuser.me/api/portraits/men/80.jpg');
+splitCostApp.addUser('Jacky', 'jacky@gmail.com', '0420400164', 'https://randomuser.me/api/portraits/men/70.jpg');
+splitCostApp.addNewUserEventListener();
